89b432a0b3456c989f3ed8271a5f3333578b37b0
Artemfrontend <kovalenko.front.end@gmail.com>
2018-03-17 18:19:18 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,1 @@
+function memo_onkeydown(){var e,n;e=memo.value,n=0+e.length,countlabel.innerText=n}document.getElementById("click").onclick=function(){var e=document.getElementById("form");e.classList.add("forma2")},document.getElementById("click2").onclick=function(){var e=document.getElementById("form");e.classList.remove("forma2")};
